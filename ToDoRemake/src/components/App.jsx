abbaebef64b2f93bc2f2d1cebdcfe32a11c4a425
GrannyYetta <you@example.com>
2023-08-26 19:09:00 +0200

    handled State

--- ToDoRemake/src/components/App.jsx
+++ ToDoRemake/src/components/App.jsx
@@ -1,15 +1,22 @@
-
-
+import React, {useState} from "react"
 
 function App () {
+
+    const [inputText, setInputText] = useState("")
+
+    function handleChange(event) {
+        const newValue = event.target.value
+        setInputText(newValue)
+    }
+
     return (
         <div className="container">
             <div className="heading">
                 <h1>List of things to do</h1>
             </div>
             <div className="form">
-                <input type="text" />
-                <button>
+                <input type="text" value={inputText}/>
+                <button onClick={handleChange}>
                     <span>Add</span>
                     </button>
                     </div>
