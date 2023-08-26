3e3eabf69a19b3cf6be5b324dd0ffb26134d28c1
GrannyYetta <you@example.com>
2023-08-26 18:46:55 +0200

    setup and boilerplate

---
+++ ToDoRemake/src/components/App.jsx
@@ -0,0 +1,25 @@
+
+
+
+function App () {
+    return (
+        <div className="container">
+            <div className="heading">
+                <h1>List of things to do</h1>
+            </div>
+            <div className="form">
+                <input type="text" />
+                <button>
+                    <span>Add</span>
+                    </button>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>An Item</li>
+                        </ul>
+                    </div>
+        </div>
+    )
+}
+
+export default App
